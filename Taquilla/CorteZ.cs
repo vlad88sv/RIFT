227c0d54cb4391f9aa530992914313442a9a72dd
Vladimir Hidalgo <vlad@todosv.com>
2011-08-16 22:43:34 -0600

    Fix: crash si no hay compras

--- Taquilla/CorteZ.cs
+++ Taquilla/CorteZ.cs
@@ -34,11 +34,16 @@ namespace Taquilla
 
 			lblTotal.Text = "[COMBINADO: $" + TotalDelDia.ToString("0.00") + "] [JUEGO $" + totalJuegos.ToString("0.00") + "] [EVENTOS $" + totalEventos.ToString("0.00") + "] [CAFETERIA $" + totalCafeteria.ToString("0.00") + "]";
 			
-			MySQL.consultar("SELECT COUNT(*) AS 'numero_de_compras', SUM(total_compra) AS 'total_de_compras' FROM cafeteria_ingresos WHERE DATE(fechatiempo) = '"+fechaDiaTrabajoFMySQL+"'");
-			MySQL.Reader.Read();
+			string numero_de_compras = "0", total_de_compras = "0.00";
+			if (MySQL.consultar("SELECT COUNT(*) AS 'numero_de_compras', SUM(total_compra) AS 'total_de_compras' FROM cafeteria_ingresos WHERE DATE(fechatiempo) = '"+fechaDiaTrabajoFMySQL+"'"))
+			{
+				MySQL.Reader.Read();
+				numero_de_compras = MySQL.Reader["numero_de_compras"].ToString();
+				total_de_compras = MySQL.Reader["total_de_compras"].ToString();
+			}
 			
-			txtNoCompras.Text = MySQL.Reader["numero_de_compras"].ToString();
-			txtTotalCompras.Text = MySQL.Reader["total_de_compras"].ToString();
+			txtNoCompras.Text = numero_de_compras;
+			txtTotalCompras.Text = total_de_compras;
 		}
 		
 		protected virtual void OnButtonOkClicked (object sender, System.EventArgs e)
