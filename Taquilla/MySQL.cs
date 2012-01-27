3fc8206502a712b51e068ad1442dc2fb86d3e1c4
Vladimir Hidalgo <vlad@todosv.com>
2012-01-27 17:09:56 -0600

    Reforzar wrapper personalizado de MySQL
    Protegerlo ante la mayoría de desconexiones, asi como la mayoría de casos de servidor MySQL no disponible

--- Taquilla/MySQL.cs
+++ Taquilla/MySQL.cs
@@ -10,32 +10,58 @@ public static class MySQL
 	private static MySql.Data.MySqlClient.MySqlConnection MyCon = new MySql.Data.MySqlClient.MySqlConnection(MyConString);
 	private static MySql.Data.MySqlClient.MySqlCommand MyCmd = new MySql.Data.MySqlClient.MySqlCommand();
 	public static MySqlDataReader Reader;
-	static MySQL()
-	{
-		IniciarConexionMySQL();
-	}
 	
-	private static void IniciarConexionMySQL()
+	private static bool IniciarConexionMySQL()
 	{
-		if (Reader != null && Reader.IsClosed == false)
+		
+		if (Reader != null)
+		{
+			Console.WriteLine("MySQL.IniciarConexion :: Reader.Close");
 			Reader.Close();
+		}
 		
-		if (MyCon.Ping() == false && MyCon.State == ConnectionState.Open)
-			MyCon.Close();
+		/*
+		if (Reader != null)
+		Console.WriteLine("MySQL.IniciarConexion :: Reader.IsClosed : " + Reader.IsClosed);
+		Console.WriteLine("MySQL.IniciarConexion :: MyCon.Ping : " + MyCon.Ping());
+		Console.WriteLine("MySQL.IniciarConexion :: MyCon.State : " + ConnectionState.Open);
+		*/
 		
-		if (MyCon.State == ConnectionState.Open)
-			return;
+		if (MyCon.Ping() == true && MyCon.State == ConnectionState.Open)
+		{
+			Console.WriteLine("MySQL.IniciarConexion :: <--- MYSQL LISTO --->");
+			return true;
+		}
+	
+		Console.WriteLine("MySQL.IniciarConexion :: --- INICIANDO MYSQL ---");
 		
-		MyCon.Open();
-		MyCmd.Connection = MyCon;
+		try {
+			if (MyCon.State != ConnectionState.Open)
+				MyCon.Open();
+			MyCmd.Connection = MyCon;
+		} catch (MySqlException e) {
+			Console.WriteLine ("MySQL.Error :: #" + e.Number + " = " + e.Message);
+			return false;
+		}
+	
+		Console.WriteLine("MySQL.IniciarConexion :: --- CONFIGURANDO MYSQL --->");
+		
 		consultar("SET LC_TIME_NAMES='es_SV'; SET NAMES UTF8; SET CHARACTER SET UTF8;");
+		Reader.Close();
+		
+		Console.WriteLine("MySQL.IniciarConexion :: <--- MYSQL CONFIGURADO ---");
+		return true;
 	}
 	
  	public static bool consultar(string query)
 	{
-		IniciarConexionMySQL();
 		double benchmark = DateTime.Now.TimeOfDay.TotalMilliseconds;
-		Console.WriteLine (query);
+		Console.WriteLine ("MySQL.Consultar :: =================== + INICIO + ===============================");
+		Console.WriteLine ("MySQL.Consultar.Query :: " + query);
+		
+		if (!IniciarConexionMySQL())
+			return false;
+		
 		try {
 			MyCmd.CommandText = query;
 			Reader = MyCmd.ExecuteReader();
@@ -45,6 +71,7 @@ public static class MySQL
 			return false;
 		}
 		Console.WriteLine("MySQL.Benchmark :: " + (DateTime.Now.TimeOfDay.TotalMilliseconds-benchmark));
+		Console.WriteLine ("MySQL.Consultar :: ===================== + FIN + =============================");
 		return true;
 	}
 }
